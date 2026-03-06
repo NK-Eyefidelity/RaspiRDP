cc8fe0dd0a06125668bbdbdcee9ab865986c0974
NK-Eyefidelity <nils.kemmann@eyefidelity.it>
2026-03-06 13:17:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,21 +8,25 @@ sudo apt-get install -y freerdp2-x11 yad wget
 sudo raspi-config nonint do_wayland W1
 
 # SH-Skript und PNG über Platzhalter-URLs herunterladen
-wget -O "$HOME/rdp.sh" "https://github.com/NK-Eyefidelity/RaspiRDP/blob/0fcc80bd5032324ca8af66ca0c7c2638352e6a6d/rdp.sh"
-wget -O "$HOME/icon.png" "https://github.com/NK-Eyefidelity/RaspiRDP/blob/0fcc80bd5032324ca8af66ca0c7c2638352e6a6d/eye.png"
+TARGET_DIR="/home/administrator/"
+
+wget -O "$TARGET_DIR/rdp.sh" "https://github.com/NK-Eyefidelity/RaspiRDP/blob/0fcc80bd5032324ca8af66ca0c7c2638352e6a6d/rdp.sh"
+wget -O "$TARGET_DIR/icon.png" "https://github.com/NK-Eyefidelity/RaspiRDP/blob/0fcc80bd5032324ca8af66ca0c7c2638352e6a6d/eye.png"
 
 # Das heruntergeladene Skript ausführbar machen
-chmod +x "$HOME/rdp.sh"
+chmod +x "$TARGET_DIR/rdp.sh"
 
 # Autostart-Eintrag für LXDE/Openbox erstellen
-AUTOSTART_DIR="$HOME/.config/autostart"
+AUTOSTART_DIR="$TARGET_DIR/.config/autostart"
 mkdir -p "$AUTOSTART_DIR"
+
+chown -R administrator:administrator $TARGET_DIR $TARGET_DIR/.config
 
 cat <<EOF > "$AUTOSTART_DIR/kiosk_autostart.desktop"
 [Desktop Entry]
 Type=Application
 Name=KioskAutostart
-Exec=$HOME/rdp.sh
+Exec=$TARGET_DIR/rdp.sh
 Terminal=false
 EOF
 
