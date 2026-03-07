eb758b9f0805999d9d5f930c770d78c4deb8ee6c
NK-Eyefidelity <nils.kemmann@eyefidelity.it>
2026-03-07 18:04:47 +0100

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -191,7 +191,7 @@ while true; do
     echo "$USERNAME" > "$USER_FILE"
 
     # xfreerdp starten
-    xfreerdp /v:"$RDP_IP" /d:"$RDP_DOMAIN" /u:"$USERNAME" /p:"$PASSWORD" /multitransport -bitmap-cache -glyph-cache /sec:nla /gdi:hw -fonts +gfx-thin-client /gfx:rfx /network:auto /cert-ignore /f
+    xfreerdp3 /v:"$RDP_IP" /d:"$RDP_DOMAIN" /u:"$USERNAME" /p:"$PASSWORD" /multitransport /cache:bitmap:off,glyph:off /sec:nla /gdi:hw -fonts /gfx:thin-client,rfx /network:auto /cert:ignore /f
 
     RDP_EXIT=$?
 
